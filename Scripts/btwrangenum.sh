3b3aa5fd5569f96f2b84fa476598a86c3c046051
Vaibhav Gupta <vaibhavguptaxyz@gmail.com>
2020-08-13 12:21:16 +0530

    Initial commit

---
+++ Scripts/btwrangenum.sh
@@ -0,0 +1,11 @@
+echo "Enter number between 10 and 20:\c"
+read num
+if [ $num -lt 10]
+then 
+echo "Number is below the belt"
+elif [ $num -gt 20]
+then 
+echo "Number is above the belt"
+else
+echo "Now you make sence"
+fi
