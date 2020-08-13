aa46b37c880f8d31c81d8cefa7e8dd63051a2af5
Vaibhav Gupta <vaibhavguptaxyz@gmail.com>
2020-08-13 13:13:06 +0530

    Update for the script file empty or not

--- Scripts/fileemptyornot.sh
+++ Scripts/fileemptyornot.sh
@@ -2,7 +2,7 @@ echo "Please provide me the name of file"
 read $fname
 if [ -s $fname]
 then 
-    echo "File is empty"
-else 
     echo "File is not empty"
+else 
+    echo "File is empty"
 fi          
