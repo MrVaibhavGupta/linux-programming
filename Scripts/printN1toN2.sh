3e35cd4032a14a03fe70578cf729790af6967961
Vaibhav Gupta <vaibhavguptaxyz@gmail.com>
2020-08-13 19:53:51 +0530

    Updated the print number script

--- Scripts/printN1toN2.sh
+++ Scripts/printN1toN2.sh
@@ -1,7 +1,7 @@
 read -p "Enter the starting number: " n1
 read -p "Enter the ending number: " n2
 count=$n1
-while [ $count -le 10 ]
+while [ $count -le $n2 ]
 do
     echo $count
     count=`expr $count +1`
