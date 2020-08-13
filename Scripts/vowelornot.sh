d7385b78d7b5d3ff30e18aa0f740895c27043c2e
Vaibhav Gupta <vaibhavguptaxyz@gmail.com>
2020-08-13 17:19:21 +0530

    Vowel or not shell script

---
+++ Scripts/vowelornot.sh
@@ -0,0 +1,12 @@
+read -p "Enter the lowercase character :" char
+if [ `echo $char | wc -c` -eq 2 ]
+then
+    if [ $char = a -o $char = e -o $char = i -o $char=o -o $char=u ]
+    then
+        echo "Entered character is a vowel"
+    else
+        echo "Entered character is not vowel"
+    fi    
+else
+    echo "Invalid Input" 
+fi
