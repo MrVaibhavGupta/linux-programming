ffcddb2dbaac07b24d78941499d1a076be86f9ca
Vaibhav Gupta <vaibhavguptaxyz@gmail.com>
2020-08-14 11:30:10 +0530

    Table shell script

---
+++ Scripts/table.sh
@@ -0,0 +1,10 @@
+read -p "Digit of which table needs to be printed: " n1
+read -p "Till when table is required: " n2
+count=1
+table=$n1
+until [ $count = $n2 ]
+do
+    echo $table
+    count=`expr $count + 1`
+    table=`expr $table \* $n1`
+done
