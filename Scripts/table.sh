4adf20a9621bda07d5918abb2b6956e65648f052
Vaibhav Gupta <vaibhavguptaxyz@gmail.com>
2020-08-14 11:40:06 +0530

    table script correction

--- Scripts/table.sh
+++ Scripts/table.sh
@@ -2,9 +2,9 @@ read -p "Digit of which table needs to be printed: " n1
 read -p "Till when table is required: " n2
 count=1
 table=$n1
-until [ $count = $n2 ]
+until [ $count -ge $n2 ]
 do
     echo $table
     count=`expr $count + 1`
-    table=`expr $table \* $count`
+    table=`expr $n1 \* $count`
 done
