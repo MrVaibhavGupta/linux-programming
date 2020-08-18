311b7074611486cb3496d44f2941b2bb11ea11ac
Vaibhav Gupta <vaibhavguptaxyz@gmail.com>
2020-08-18 13:40:04 +0530

    login report shell script

---
+++ Scripts/loginreport.sh
@@ -0,0 +1,23 @@
+read -p "Enter the username: " logname
+grep "$logname" /etc/passwd > /dev/null
+if [ $? -eq 0 ]
+then 
+    time=0
+    who | grep "$logname" > /dev/null
+    if [ $? -eq 0 ]
+    then
+        echo "$logname has logged in."
+        if [ $time -ge 60 ]  
+        then
+            min=`expr $time / 60`
+            sec=`expr $time % 60`
+            echo "and is $min minute $sec second late "
+        elif [ $time -lt 60 -a $time -ne 0 ]
+        then
+            echo "and is $time second late"
+
+        fi
+    fi  
+else 
+    echo "Invalid username"
+fi
